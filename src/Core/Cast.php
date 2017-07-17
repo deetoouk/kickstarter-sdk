54132eb6e0475587c0127b7d18c0d96864c89e8a
Jordan Dobrev <jordan.dobrev@livebuzz.co.uk>
2017-07-17 14:58:29 +0100

    tidy up

---
+++ src/Core/Cast.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace JTDSoft\EssentialsSdk\Core;
+
+use JTDSoft\EssentialsSdk\Exceptions\ErrorException;
+
+/**
+ * Class Cast
+ *
+ * @package JTDSoft\EssentialsSdk\Core
+ */
+class Cast
+{
+    /**
+     * @param string $cast
+     * @param        $response
+     *
+     * @return mixed
+     */
+    public static function single(string $cast, $response)
+    {
+        return new $cast($response);
+    }
+
+    /**
+     * @param      $response
+     * @param null $cast
+     *
+     * @return mixed
+     * @throws ErrorException
+     */
+    public static function castMany($cast, $response)
+    {
+        $result = [];
+
+        if (!$response) {
+            return $result;
+        }
+
+        if (isset($response['total']) && isset($response['data'])) { //for paging
+            $paging = new Paging();
+
+            $paging->setPage($response['current_page']);
+            $paging->setTotal($response['total']);
+            $paging->setLastPage($response['last_page']);
+            $paging->setFrom($response['from']);
+            $paging->setTo($response['to']);
+            $paging->setItems(static::castMany($cast, $response['data']));
+
+            return $paging;
+        }
+
+        foreach ($response as $key => $value) {
+            $result[] = self::cast($cast, $value);
+        }
+
+        return new Collection($result);
+    }
+}
--- src/Core/Service.php
+++ src/Core/Service.php
@@ -4,7 +4,7 @@ namespace JTDSoft\EssentialsSdk\Core;
 
 use GuzzleHttp\Client as Guzzle;
 use JTDSoft\EssentialsSdk\Contracts\Client;
-use JTDSoft\EssentialsSdk\Exceptions\ErrorException;
+use JTDSoft\EssentialsSdk\Core\Traits\SupportsHeaders;
 
 /**
  * Class Service
@@ -13,140 +13,12 @@ use JTDSoft\EssentialsSdk\Exceptions\ErrorException;
  */
 class Service extends Config
 {
+    use SupportsHeaders;
+
     /**
      * @var Client
      */
     protected $client;
-
-    /**
-     * @var array
-     */
-    protected $headers;
-
-    /**
-     * @return mixed
-     */
-    public function getHeaders()
-    {
-        return $this->headers ?: [];
-    }
-
-    /**
-     * @param array $headers
-     *
-     * @return $this
-     */
-    public function setHeaders(array $headers)
-    {
-        $this->headers = $headers;
-
-        return $this;
-    }
-
-    /**
-     * @param string $header
-     * @param string $value
-     */
-    public function setHeader(string $header, string $value)
-    {
-        $this->headers[$header] = $value;
-    }
-
-    /**
-     * @param string $header
-     * @param string $value
-     */
-    public function setCustomHeader(string $header, string $value)
-    {
-        $this->headers[self::$custom_header_prefix . $header] = $value;
-    }
-
-    /**
-     * Adds all headers
-     */
-    protected function prepareHeaders()
-    {
-        $this->setCustomHeader('Version', static::getVersion());
-        $this->setHeader('Accept-Language', static::getLanguage());
-        $this->setHeader('Authorization', 'Bearer ' . static::getApiKey());
-    }
-
-    /**
-     * @param string $cast
-     * @param        $response
-     *
-     * @return mixed
-     */
-    public final function cast(string $cast, $response)
-    {
-        return new $cast($response);
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function callAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->call(...$parameters));
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function getAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->get(...$parameters));
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function postAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->post(...$parameters));
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function putAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->put(...$parameters));
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function patchAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->put(...$parameters));
-    }
-
-    /**
-     * @param       $cast
-     * @param array ...$parameters
-     *
-     * @return mixed
-     */
-    public final function deleteAndCast($cast, ...$parameters)
-    {
-        return $this->cast($cast, $this->delete(...$parameters));
-    }
 
     /**
      * @param       $verb
@@ -159,7 +31,7 @@ class Service extends Config
      * @throws \JTDSoft\EssentialsSdk\Exceptions\ServerException
      * @throws \JTDSoft\EssentialsSdk\Exceptions\UnauthorizedException
      */
-    public final function call($verb, $method, array $request = null)
+    protected final function call($verb, $method, array $request = null)
     {
         if (is_null($request)) {
             $request = [];
@@ -250,51 +122,5 @@ class Service extends Config
         );
 
         return $endpoint;
-    }
-
-    /**
-     * @param string $cast
-     * @param array  ...$parameters
-     *
-     * @return mixed
-     */
-    public final function callAndCastMany($cast, ...$parameters)
-    {
-        return $this->castMany($cast, $this->call(...$parameters));
-    }
-
-    /**
-     * @param      $response
-     * @param null $cast
-     *
-     * @return mixed
-     * @throws ErrorException
-     */
-    public final function castMany($cast, $response)
-    {
-        $result = [];
-
-        if (!$response) {
-            return $result;
-        }
-
-        if (isset($response['total']) && isset($response['data'])) { //for paging
-            $paging = new Paging();
-
-            $paging->setPage($response['current_page']);
-            $paging->setTotal($response['total']);
-            $paging->setLastPage($response['last_page']);
-            $paging->setFrom($response['from']);
-            $paging->setTo($response['to']);
-            $paging->setItems($this->castMany($cast, $response['data']));
-
-            return $paging;
-        }
-
-        foreach ($response as $key => $value) {
-            $result[] = self::cast($cast, $value);
-        }
-
-        return new Collection($result);
     }
 }
